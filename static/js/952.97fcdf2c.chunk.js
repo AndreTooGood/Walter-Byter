b2168f71b6841b2c4be45a75f2319bc0f9130530
AndreTooGood <andrejohnv@gmail.com>
2024-02-25 13:29:04 +0530

    Updates

---
+++ static/js/952.97fcdf2c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwiz_buy_web=self.webpackChunkwiz_buy_web||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.97fcdf2c.chunk.js.map
